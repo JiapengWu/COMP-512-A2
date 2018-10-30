0cccf4f64814c30cd322b10bd900b7b05e0d432b
JiapengWu <jiapeng.wu@mail.mcgill.ca>
2018-10-30 09:48:08 -0400

    WIP

--- env_setup.sh
+++ env_setup.sh
@@ -1,4 +1,4 @@
 export HOMEDIR=`pwd`
 export JARPATH=${HOMEDIR}/target/COMP-512-Project2-0.0.1-SNAPSHOT-jar-with-dependencies.jar
-echo -e "grant codebase \"file:`pwd`/src/main/java/Client\" {\npermission java.security.AllPermission;\n};" > client.policy
-echo -e "grant codebase \"file:`pwd`/src/main/java/Server\" {\npermission java.security.AllPermission;\n};" > server.policy
+echo -e "grant codebase \"file:`pwd`/src/main/java/Client/\" {\npermission java.security.AllPermission;\n};" > client.policy
+echo -e "grant codebase \"file:`pwd`/src/main/java/Server/\" {\npermission java.security.AllPermission;\n};" > server.policy
